fdc0872944c7d49fc98b616c89cc864eb2349713
anoopps <anoopps1111@gmail.com>
2018-09-26 20:13:28 +0530

    script complete

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-
-	jQuery('#search-txt').bind( "keypress keyup", function() {
-        
-		var searchtxt = jQuery(this).val().toLowerCase();
-         
+jQuery(document).ready(function(){    
+    jQuery('#search-txt').bind( "keypress keyup", function() {        
+		var searchtxt = jQuery(this).val().toLowerCase();         
 			jQuery('ul#sortable > li').hide();		
 			var items = jQuery('ul#sortable > li');	    
 			jQuery.each(items,function(index, node){				    
@@ -15,4 +13,25 @@
 	jQuery("#search_btn").click(function(){		
 		jQuery('#search-txt').val('');
 		jQuery('#search-txt').trigger('keypress');
-	});	
+	});    
+    jQuery('.deleteme').click(function(e){                              
+        var check = confirm("Are you sure you want to delete?");
+        if(check){
+            jQuery(this).closest('.ui-state-default').remove();
+        }
+    });    
+    jQuery('#additem').click(function(){
+         var panel = prompt("Enter the name of the panel", "");
+        panel = jQuery.trim(panel);
+            if (panel!="") {
+                $("ul#sortable")  
+                .find('li:first') 
+                .clone()  
+                .find('.element')  
+                .text(panel)  
+                .end()  
+                .appendTo('ul#sortable');
+            }
+        });
+});
+	
