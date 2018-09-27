d251a305f40ee85cf9a8ebaeb2ca6acee77002ca
Anoop <anoopps1111@gmail.com>
2018-09-27 22:19:49 +0530

    Final modifications

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-jQuery(document).ready(function(){    
+jQuery(document).ready(function(){
+    jQuery('.tabspane > div').hide();
+    jQuery('.tabspane > div').first().show();
+    jQuery('.clear').hide();
+    
     jQuery('#search-txt').bind( "keypress keyup", function() {        
-		var searchtxt = jQuery(this).val().toLowerCase();         
+		var searchtxt = jQuery(this).val().toLowerCase();
+        jQuery('.clear').show();
+    
 			jQuery('ul#sortable > li').hide();		
 			var items = jQuery('ul#sortable > li');	    
 			jQuery.each(items,function(index, node){				    
@@ -10,21 +16,27 @@ jQuery(document).ready(function(){
 				    }
 			})	
 	});
+    jQuery('.clear').click(function(){
+        jQuery('#search-txt').val('');
+        jQuery('#search-txt').trigger('keypress');
+    });
 	jQuery("#search_btn").click(function(){		
 		jQuery('#search-txt').val('');
 		jQuery('#search-txt').trigger('keypress');
-	});    
-    jQuery('.deleteme').click(function(e){        
-       var count= $( "ul#sortable li" ).length
-        if(count > 1){
-            var check = confirm("Are you sure you want to delete?");
-            if(check){
-                jQuery(this).closest('.ui-state-default').remove();
-            }
-        }else{
-            alert("You can't delete all items");
-        }        
-    });    
+	});
+    
+    $(document).on('click', '.deleteme', function(){     
+        var count= $( "ul#sortable li" ).length
+          if(count > 1){
+              var check = confirm("Are you sure you want to delete?");
+              if(check){
+                  jQuery(this).closest('.ui-state-default').remove();
+              }
+          }else{
+              alert("You can't delete all items");
+          } 
+    });
+    
     jQuery('#additem').click(function(){
          var panel = prompt("Enter the name of the panel", "");
         panel = jQuery.trim(panel);
@@ -38,5 +50,12 @@ jQuery(document).ready(function(){
                 .appendTo('ul#sortable');
             }
         });
+    
+    jQuery('#tabssection a').click(function(event){
+        var tab = jQuery(this).attr('href');
+        jQuery('.tabspane > div').hide();
+        jQuery(tab).show();
+        event.preventDefault();
+    });
 });
 	
